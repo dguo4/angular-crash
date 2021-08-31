1982841a532fc3f6099054305ad6fe2022fec6a5
dguo4 <32888712+dguo4@users.noreply.github.com>
2021-08-31 18:13:03 -0400

    add task details

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {TASKS} from "../../mock-task"
+import {TASKS} from "../../mock-task";
 import {Task} from 'src/app/Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-tasks',
@@ -9,7 +11,8 @@ import {Task} from 'src/app/Task';
 })
 export class TasksComponent implements OnInit {
   tasks: Task[] = TASKS;
-  
+  faTimes = faTimes;
+
   constructor() { }
 
   ngOnInit(): void {
